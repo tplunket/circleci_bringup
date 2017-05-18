f8e49bf4ae60fc9413a05276fad8121562c9c865
Tom Plunket <tom@mightysprite.com>
2017-05-18 00:33:37 -0700

    Cleans up binary output. Allows user to specify variable names in output.

--- ConvertToC/ConvertToC.cpp
+++ ConvertToC/ConvertToC.cpp
@@ -5,17 +5,25 @@
  * \copyright (c) 2010-2017 Tom Plunket, all rights reserved
  *
  * The data will be named k_<TranslatedName> and k_<TranslatedName>_length.
- * <TranslatedName> is the filename with dots converted to underscores.
+ * <TranslatedName> is the filename with dots converted to underscores by default.
  *
- *     python ConvertToCString.py inputfile.dat inputfile.c
+ *     ConvertToCString inputfile.dat inputfile.c
  *
  * The above line will yield two data members; k_inputfile_dat will be the file
- * contents as a const char array and k_inputfile_dat_length will be the length of
- * the file.
+ * contents as a const char array and k_inputfile_dat_length will be the length
+ * of the file.
  *
  * The character array will be zero terminated for safety's sake (as it is often
  * used for text files and prevents many circumstances of user error) but the
  * given length will be the actual length of the input file.
+ *
+ * Additional command line options are:
+ *     -b[in[ary]]: Force a "binary" export as an array of numbers instead of a
+ *                  zero-terminated string.
+ *     -h[ex]: Force binary export but use hexadecimal instead of decimal numbers
+ *             (this yields about 20% larger files).
+ *     -n[ame]: Set the name that gets used for the variables that are created
+ *              instead of using the default <TranslatedName> as defined above.
  *
  * Licensed under the MIT/X license. Do with these files what you will but leave this header intact.
  */
@@ -63,7 +71,8 @@ void GenerateTranslations(ExportType style, bool asHex=false)
         CHARACTER_TRANSLATIONS[' '] = " ";
         CHARACTER_TRANSLATIONS['"'] = "\\\"";
         CHARACTER_TRANSLATIONS['\\'] = "\\\\";
-        CHARACTER_TRANSLATIONS['\n'] = "\\n\"\n\t\"";
+        //CHARACTER_TRANSLATIONS['\n'] = "\\n\"\n\t\"";
+        CHARACTER_TRANSLATIONS['\n'] = "\\n";
         CHARACTER_TRANSLATIONS['\r'] = "\\r";
         CHARACTER_TRANSLATIONS['\t'] = "\\t";
         CHARACTER_TRANSLATIONS['\0'] = "\\0";
@@ -131,22 +140,30 @@ void GenerateTranslations(ExportType style, bool asHex=false)
     }
 }
 
-std::string ConvertDataName(std::string name)
+std::string DefaultDataName(std::string name)
 {
     size_t lastSlash = name.find_last_of("/\\");
     if (lastSlash != std::string::npos)
         name.erase(0, lastSlash+1);
     std::replace(name.begin(), name.end(), '.', '_');
-    return name;
+    return "k_" + name;
 }
 
 std::string Convert(std::string const& data)
 {
     std::string output;
+    std::string currentLine;
     for (int8_t c : data)
     {
-        output.append(CHARACTER_TRANSLATIONS[c]);
+        currentLine.append(CHARACTER_TRANSLATIONS[c]);
+        if ((c == '\n') || (currentLine.size() > 90))
+        {
+            output += currentLine;
+            output += "\"\n\t\"";
+            currentLine.erase();
+        }
     }
+    output += currentLine;
     return output;
 }
 
@@ -164,14 +181,15 @@ std::string FormatAsString(std::string const& data, std::string const& dataName)
         match = lines.find("??", match+1);
     }
     std::ostringstream oss;
-    oss << "\t\"" << lines << '"';
+    oss << "\t\"" << lines << "\";" << std::endl;
     return oss.str();
 }
 
-std::string FormatAsData(std::string const& data, std::string const& dataName)
+std::string FormatAsData(std::string data, std::string const& dataName)
 {
     std::vector<std::string> output { "{" };
 
+    data.push_back('\0');
     size_t dataLen = data.size();
     size_t current = 0;
     while (current < dataLen)
@@ -192,8 +210,7 @@ std::string FormatAsData(std::string const& data, std::string const& dataName)
 
     // put a NULL at the end in case the string is used as a string; it doesn't
     // count into the length that we printed before though.
-    output.push_back("    0");
-    output.push_back("}");
+    output.push_back("};");
     std::ostringstream actual_output;
     for (auto line : output)
     {
@@ -202,12 +219,13 @@ std::string FormatAsData(std::string const& data, std::string const& dataName)
     return actual_output.str();
 }
 
-int Main(std::string const& inputFile, std::string const& outputFile, bool asBinary, bool asHex)
+int Main(std::string const& inputFile, std::string const& outputFile, bool asBinary, bool asHex,
+         std::string const& dataName)
 {
     std::ifstream file(inputFile, std::ios::binary | std::ios::ate);
     if (!file.good())
     {
-        std::cerr << "Couldn't open file " << inputFile << "." << std::endl;
+        Error("Couldn't open file %s.", inputFile.c_str());
         return 3;
     }
     size_t size = file.tellg();
@@ -215,7 +233,6 @@ int Main(std::string const& inputFile, std::string const& outputFile, bool asBin
     std::string contents(size, 0);
     file.read(&contents[0], size);
 
-    std::string dataName = ConvertDataName(inputFile);
     std::string output;
 
     if (!asBinary && !asHex && (size < MAX_STRING_LENGTH))
@@ -232,49 +249,63 @@ int Main(std::string const& inputFile, std::string const& outputFile, bool asBin
     std::ofstream outfile(outputFile, std::ios::binary | std::ios::trunc);
     outfile << "/* This file was generated by a script and probably shouldn't be modified by hand. */" << std::endl
             << std::endl
-            << "const unsigned int k_" << dataName << "_length = " << size << ";" << std::endl
-            << "const char k_" << dataName << "[] =" << std::endl;
+            << "const unsigned int " << dataName << "_length = " << size << ";" << std::endl
+            << "const char " << dataName << "[] =" << std::endl;
 
-    outfile << output << ";" << std::endl;
+    outfile << output;
 
-	Info("%s -> %s (%d bytes)", inputFile.c_str(), outputFile.c_str(), size);
+    Info("%s -> %s (%d bytes)", inputFile.c_str(), outputFile.c_str(), size);
     return 0;
 }
 
 int main(int argc, char const** argv)
 {
-	StdStreamLogTarget lt;
+    StdStreamLogTarget lt;
 
-    char const* infile, *outfile;
-    int asBinary, asHex;
+    std::string infile, outfile, dataName;
+    bool asBinary, asHex;
 
     {
+        char const* in, *out;
+        int b, h;
+        char const* no;
+
         CommandLine cl;
-        cl.AddArgument(&infile);
-        cl.AddArgument(&outfile);
-        cl.AddCountingOption(&asBinary, "b");
-        cl.AddCountingOption(&asBinary, "bin");
-        cl.AddCountingOption(&asBinary, "binary");
-        cl.AddCountingOption(&asHex, "h");
-        cl.AddCountingOption(&asHex, "hex");
+        cl.AddArgument(&in);
+        cl.AddArgument(&out);
+        cl.AddCountingOption(&b, "b");
+        cl.AddCountingOption(&b, "bin");
+        cl.AddCountingOption(&b, "binary");
+        cl.AddCountingOption(&h, "h");
+        cl.AddCountingOption(&h, "hex");
+        cl.AddStringOption(&no, "n");
+        cl.AddStringOption(&no, "name");
+
         if (!cl.Parse(argc, argv))
         {
-			// if Parse fails, the log target gets the messages.
+            // if Parse fails, the log target gets the messages.
             return 1;
         }
 
-        if (infile == nullptr)
-		{
-            std::cerr << "Need to give a filename." << std::endl;
-			return 2;
-		}
+        if (in == nullptr)
+        {
+            Error("Need to give a filename.");
+            return 2;
+        }
+
+        infile = in;
+        if (out != nullptr)
+            outfile = out;
+        else
+            outfile = infile + ".c";
+
+        asBinary = b != 0;
+        asHex = h != 0;
+        if (no != nullptr)
+            dataName = no;
+        else
+            dataName = DefaultDataName(infile);
     }
 
-    std::string sinfile = infile;
-    std::string soutfile;
-    if (outfile != nullptr)
-        soutfile = outfile;
-    else
-        soutfile = sinfile + ".c";
-    return Main(sinfile, soutfile, asBinary!=0, asHex!=0);
+    return Main(infile, outfile, asBinary, asHex, dataName);
 }
