d701fbf5a23eec35b7fb5e933b0b6c09139a9f93
Tom Plunket <tom@mightysprite.com>
2018-08-23 21:55:01 -0700

    Makes test.bat more easily configurable.

--- test.bat
+++ test.bat
@@ -1,13 +1,38 @@
 @echo off
 setlocal
 
-set GENERATOR="Visual Studio 14 2015 Win64"
+set "COMPILER=Visual Studio"
+
+::set "COMPILER_VERSION=11 2012"
+set "COMPILER_VERSION=14 2015"
+::set "COMPILER_VERSION=15 2017"
+
+set COMPILER_ARCHITECTURE=Win64
+::set COMPILER_ARCHITECTURE=ARM
+
 set CONFIG=Debug
+set CONFIG=Release
+
+if "%COMPILER_ARCHITECTURE%"=="" (
+    set GENERATOR="%COMPILER% %COMPILER_VERSION%"
+) else (
+    set GENERATOR="%COMPILER% %COMPILER_VERSION% %COMPILER_ARCHITECTURE%"
+)
 
 pushd %~dp0
 
 set "PROJECT=%~1"
-set PROJECT_DIR="_out\%~1"
+set PROJECT_DIR="_out\%~1\vs%COMPILER_VERSION:~0,2%.%COMPILER_ARCHITECTURE%"
+
+if "%PROJECT%"=="" (
+    echo Must specify a project name on the command line.
+    exit /b 1
+)
+
+if NOT EXIST "%PROJECT%" (
+    echo Project named %PROJECT% does not appear to exist.
+    exit /b 2
+)
 
 if "%PROJECT%"=="Log" set EXTRA=-DUSE_wchar_t=off
 
@@ -15,6 +40,6 @@ if NOT EXIST %PROJECT_DIR% md %PROJECT_DIR%
 
 echo on
 cd %PROJECT_DIR%
-cmake -G %GENERATOR% %EXTRA% "..\..\%PROJECT%\" && cmake --build . --config %CONFIG% -j && ".\%CONFIG%\%PROJECT%Tests"
+cmake -G %GENERATOR% %EXTRA% "..\..\..\%PROJECT%\" && cmake --build . --config %CONFIG% -j && ".\%CONFIG%\%PROJECT%Tests"
 
 popd
